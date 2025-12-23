27217183b8750b121b4f49474fa31017b529d22a
klgaadnwdeh <163373392+klgaadnwdeh@users.noreply.github.com>
2025-12-23 16:30:43 +0800

    Update rloo.py

--- trl_fix/rloo.py
+++ trl_fix/rloo.py
@@ -159,8 +159,8 @@ def reward(labels, completions):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_path', type=str, default=r"../model/think/..")
-    parser.add_argument('--train_data_path', type=str, default=r"../data/think/")
-    parser.add_argument('--valid_data_path', type=str, default=r"../data/think/..")
+     parser.add_argument('--train_data_path', type=str, default=r"../data/think/data1/train_data")
+    parser.add_argument('--valid_data_path', type=str, default=r"../data/think/data1/valid_data")
     parser.add_argument('--logging_steps', type=int, help="the logging frequency.", default=10)
     parser.add_argument('--save_steps', type=int, help="the saving frequency.", default=26000)
     parser.add_argument('--learning_rate', type=float, help="The initial learning rate for [`AdamW`] optimizer.",
@@ -185,7 +185,7 @@ if __name__ == "__main__":
                         help="Total number of episodes in the dataset.",
                         default=35000)
     parser.add_argument('--output_path', type=str, help="Output path for the trained model",
-                        default="../model/think//data1/RLOO")
+                        default="../model/think/RLOO/data1/model")
     args = parser.parse_args()
     training_args = RLOOConfig(save_steps=args.save_steps,
                                output_dir=args.output_path,
